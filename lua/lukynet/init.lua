fd6d9d3429ae981ae95a31b75d6a0ac1d4f3fd52
lukynet <98685127+LukyNet21@users.noreply.github.com>
2025-02-03 11:16:27 +0100

    updating sets

--- lua/lukynet/init.lua
+++ lua/lukynet/init.lua
@@ -1,11 +1,4 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.signcolumn = 'yes'
-vim.opt.nu = true
-vim.opt.relativenumber = true
+require("lukynet.set")
 require("lukynet.lazy")
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
