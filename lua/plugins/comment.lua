9dcdebd4ecb2acf4c8ab309d3758ef015f5892ea
lukynet <98685127+LukyNet21@users.noreply.github.com>
2025-03-28 11:23:48 +0100

    adding comments and some other plugins

---
+++ lua/plugins/comment.lua
@@ -0,0 +1,18 @@
+return {
+  'numToStr/Comment.nvim',
+  opts = {
+    toggler = {
+      line = "<leader>/",
+      block = "<leader>/",
+    },
+    opleader = {
+      line = "<leader>/",
+      block = "<leader>/",
+    },
+    extra = {
+      above = "<leader>O",
+      below = "<leader>o",
+      eol   = "<leader>A",
+    },
+  },
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
   'nvim-treesitter/nvim-treesitter',
   build = ":TSUpdate",
   opts = {
-    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "javascript", "typescript", "go", "python", "astro", "bash", "css", "go" },
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "javascript", "typescript", "go", "python", "astro", "bash", "css", "yaml", "json", "html", "tsx" },
     sync_install = false,
     auto_install = true,
     highlight = {
